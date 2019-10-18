b0ee09e0f2c28abff9d0de94ce002312a9227baf
steamBOT <steam@ex41-hel-32>
2019-10-18 19:58:14 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Start server
 cd src/rust
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`$SCRIPT_DIR/RustDedicated_Data/Plugins/x86_64
-timeout 1m ./RustDedicated -batchmode -nographics \
+timeout 2m ./RustDedicated -batchmode -nographics \
     +server.hostname "Test server" \
     +server.seed 1234 \
     +server.worldsize 1000 \
